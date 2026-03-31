70cc0adffc7141d8de1f8cc4781a7bf042028ab8
Parag More <34959548+paragmore@users.noreply.github.com>
2026-03-31 11:40:02 +0530

    Add override to queryThing in miner workers (#14)
    * Add override to queryThing in miner workers
    
    * migrate hook usage to tpl thing

--- workers/rack.miner.wrk.js
+++ workers/rack.miner.wrk.js
@@ -39,10 +39,12 @@ class WrkMinerRack extends WrkRack {
     return getRandomString(15)
   }
 
-  async syncPoolConfig () {
-    for (const thg of Object.values(this.mem.things)) {
-      if (thg.ctrl?.poolConfig && thg.info.poolConfig !== thg.ctrl.poolConfig) {
-        thg.info.poolConfig = thg.ctrl.poolConfig
+  async _queryThingHook (req, res) {
+    if (req.method === 'setupPools' && res?.success) {
+      const thg = this.mem.things[req.id]
+      const configId = thg.ctrl.poolConfig
+      if (thg && configId && thg.info.poolConfig !== configId) {
+        thg.info.poolConfig = configId
         await this.saveThingData(thg)
       }
     }
@@ -61,12 +63,6 @@ class WrkMinerRack extends WrkRack {
           ['updateThing', 1],
           ['forgetThings', 1]
         ])
-
-        this.interval_0.add(
-          'syncPoolConfig',
-          this.syncPoolConfig.bind(this),
-          5000
-        )
 
         next()
       }
